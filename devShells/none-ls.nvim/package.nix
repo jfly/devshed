117e2fc27879032e5525fcf7583da40bee2c5f12
Jeremy Fleischman <me@jfly.fyi>
2026-06-02 22:51:41 -0700

    Add note about testing with your own build of neovim

--- devShells/none-ls.nvim/package.nix
+++ devShells/none-ls.nvim/package.nix
@@ -4,12 +4,18 @@
 }:
 
 mkShell {
+  shellHook = ''
+    echo -e "\e[0;32mWelcome to a none-ls.nvim dev shell!\e[0m"
+    echo -e "\e[0;32mTo test with your own build of neovim, add the following to your .envrc:\e[0m"
+    echo 'export VIMRUNTIME=$HOME/src/github.com/neovim/neovim/runtime'
+    echo 'PATH_add "$HOME/src/github.com/neovim/neovim/build/bin"'
+  '';
   packages = with pkgs; [
     gnumake
     yq
     pre-commit
     write-good
-    nodePackages.prettier
+    prettier
     markdownlint-cli
     luajitPackages.tl
   ];
